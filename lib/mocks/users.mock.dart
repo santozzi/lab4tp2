4cd6d9d24cc08aae55d4e468b68f923d49153871
Sergio <santozzi@gmail.com>
2024-11-12 22:58:17 -0300

    change structure

---
+++ lib/mocks/users.mock.dart
@@ -0,0 +1,253 @@
+const List users = [
+  {
+    "id": 1,
+    "email": "john@mail.com",
+    "password": "changeme",
+    "name": "Jhon",
+    "role": "customer",
+    "avatar": "https://i.imgur.com/LDOO4Qs.jpg",
+    "creationAt": "2024-11-12T03:05:16.000Z",
+    "updatedAt": "2024-11-12T03:05:16.000Z"
+  },
+  {
+    "id": 2,
+    "email": "maria@mail.com",
+    "password": "12345",
+    "name": "Maria",
+    "role": "customer",
+    "avatar": "https://i.imgur.com/DTfowdu.jpg",
+    "creationAt": "2024-11-12T03:05:16.000Z",
+    "updatedAt": "2024-11-12T03:05:16.000Z"
+  },
+  {
+    "id": 3,
+    "email": "admin@mail.com",
+    "password": "admin123",
+    "name": "Admin",
+    "role": "admin",
+    "avatar": "https://i.imgur.com/yhW6Yw1.jpg",
+    "creationAt": "2024-11-12T03:05:16.000Z",
+    "updatedAt": "2024-11-12T03:05:16.000Z"
+  },
+  {
+    "id": 4,
+    "email": "mnov2288@gmail.com",
+    "password": "andeca88",
+    "name": "Dapur MakNov",
+    "role": "customer",
+    "avatar": "https://api.lorem.space/image/face?w=640&h=480",
+    "creationAt": "2024-11-12T03:20:50.000Z",
+    "updatedAt": "2024-11-12T03:20:50.000Z"
+  },
+  {
+    "id": 5,
+    "email": "elqloleaes@puto.com",
+    "password": "laconchadelalora",
+    "name": "Adrianchooo pancho",
+    "role": "customer",
+    "avatar": "https://picsum.photos/800",
+    "creationAt": "2024-11-12T04:36:21.000Z",
+    "updatedAt": "2024-11-12T04:36:21.000Z"
+  },
+  {
+    "id": 7,
+    "email": "elqloleaes@puto.com",
+    "password": "laconchadelalora",
+    "name": "Adrianchooo pancho",
+    "role": "customer",
+    "avatar": "https://picsum.photos/800",
+    "creationAt": "2024-11-12T04:37:17.000Z",
+    "updatedAt": "2024-11-12T04:37:17.000Z"
+  },
+  {
+    "id": 8,
+    "email": "jayesh@mbncard.com",
+    "password": "123456",
+    "name": "Jayesh",
+    "role": "customer",
+    "avatar": "https://picsum.photos/800",
+    "creationAt": "2024-11-12T04:54:23.000Z",
+    "updatedAt": "2024-11-12T04:54:23.000Z"
+  },
+  {
+    "id": 9,
+    "email": "saturnianin777@gmail.com",
+    "password": "12345",
+    "name": "Тимоша Солнышко",
+    "role": "customer",
+    "avatar":
+        "https://images.panda.org/assets/images/pages/welcome/orangutan_1600x1000_279157.jpg",
+    "creationAt": "2024-11-12T05:17:00.000Z",
+    "updatedAt": "2024-11-12T05:17:00.000Z"
+  },
+  {
+    "id": 10,
+    "email": "jayesh@mbncard.com",
+    "password": "123456",
+    "name": "Jayesh",
+    "role": "customer",
+    "avatar": "https://picsum.photos/800",
+    "creationAt": "2024-11-12T05:56:32.000Z",
+    "updatedAt": "2024-11-12T05:56:32.000Z"
+  },
+  {
+    "id": 11,
+    "email": "emake@gmail.com",
+    "password": "Passw0rd",
+    "name": "Emma",
+    "role": "customer",
+    "avatar": "https://www.instagram.com/immobilier.tezda/",
+    "creationAt": "2024-11-12T06:59:50.000Z",
+    "updatedAt": "2024-11-12T06:59:50.000Z"
+  },
+  {
+    "id": 12,
+    "email": "hanguyen@gmail.com",
+    "password": "hanguyen123",
+    "name": "Ha Nguyen",
+    "role": "admin",
+    "avatar": "https://api.escuelajs.co/api/v1/files/5c97.jpg",
+    "creationAt": "2024-11-12T07:45:34.000Z",
+    "updatedAt": "2024-11-12T07:49:08.000Z"
+  },
+  {
+    "id": 17,
+    "email": "Email@gmail.com",
+    "password": "123456",
+    "name": "Sam",
+    "role": "customer",
+    "avatar": "www.instagram.com/immobilier.tezda",
+    "creationAt": "2024-11-12T09:26:01.000Z",
+    "updatedAt": "2024-11-12T09:26:01.000Z"
+  },
+  {
+    "id": 18,
+    "email": "Ahmed@gmail.com",
+    "password": "1234",
+    "name": "Ahmed",
+    "role": "customer",
+    "avatar": "https://picsum.photos/800",
+    "creationAt": "2024-11-12T09:37:25.000Z",
+    "updatedAt": "2024-11-12T09:37:25.000Z"
+  },
+  {
+    "id": 19,
+    "email": "john@mail.com",
+    "password": "1234",
+    "name": "",
+    "role": "customer",
+    "avatar": "https://api.escuelajs.co/api/v1/files/8483.jpg",
+    "creationAt": "2024-11-12T10:28:58.000Z",
+    "updatedAt": "2024-11-12T12:19:50.000Z"
+  },
+  {
+    "id": 20,
+    "email": "Emeka@gmail.com",
+    "password": "12345678",
+    "name": "Emeka",
+    "role": "customer",
+    "avatar": "https://placeimg.com/640/480/any",
+    "creationAt": "2024-11-12T10:32:55.000Z",
+    "updatedAt": "2024-11-12T10:32:55.000Z"
+  },
+  {
+    "id": 21,
+    "email": "Base@gmail.com",
+    "password": "1234",
+    "name": "base",
+    "role": "customer",
+    "avatar": "www.google/image",
+    "creationAt": "2024-11-12T10:46:52.000Z",
+    "updatedAt": "2024-11-12T10:46:52.000Z"
+  },
+  {
+    "id": 22,
+    "email": "nico@gmail.com",
+    "password": "1234",
+    "name": "kat",
+    "role": "customer",
+    "avatar": "https://picsum.photos/800",
+    "creationAt": "2024-11-12T10:50:42.000Z",
+    "updatedAt": "2024-11-12T10:50:42.000Z"
+  },
+  {
+    "id": 23,
+    "email": "AS@gmail.com",
+    "password": "1234",
+    "name": "Arif Shumon",
+    "role": "customer",
+    "avatar": "https://picsum.photos/800",
+    "creationAt": "2024-11-12T11:00:53.000Z",
+    "updatedAt": "2024-11-12T11:00:53.000Z"
+  },
+  {
+    "id": 24,
+    "email": "Emeka@gmaill.com",
+    "password": "123456",
+    "name": "rose",
+    "role": "customer",
+    "avatar": "www.google.com/image",
+    "creationAt": "2024-11-12T11:18:17.000Z",
+    "updatedAt": "2024-11-12T11:18:17.000Z"
+  },
+  {
+    "id": 25,
+    "email": "nico@gmail.com",
+    "password": "1234",
+    "name": "Nicolas Test",
+    "role": "customer",
+    "avatar": "https://picsum.photos/800",
+    "creationAt": "2024-11-12T11:30:42.000Z",
+    "updatedAt": "2024-11-12T11:30:42.000Z"
+  },
+  {
+    "id": 26,
+    "email": "nico@gmail.com",
+    "password": "1234",
+    "name": "Nicolas Ray",
+    "role": "customer",
+    "avatar": "https://picsum.photos/800",
+    "creationAt": "2024-11-12T11:32:16.000Z",
+    "updatedAt": "2024-11-12T11:32:16.000Z"
+  },
+  {
+    "id": 27,
+    "email": "Nick@gmail.com",
+    "password": "1234",
+    "name": "Nick",
+    "role": "customer",
+    "avatar": "https://picsum.photos/800",
+    "creationAt": "2024-11-12T11:33:42.000Z",
+    "updatedAt": "2024-11-12T11:33:42.000Z"
+  },
+  {
+    "id": 28,
+    "email": "oluwatobi.kih@gmail.com",
+    "password": "1234",
+    "name": "oluwatobi akintola",
+    "role": "customer",
+    "avatar": "https://picsum.photos/800",
+    "creationAt": "2024-11-12T11:34:44.000Z",
+    "updatedAt": "2024-11-12T11:34:44.000Z"
+  },
+  {
+    "id": 29,
+    "email": "Nick@gmail.com",
+    "password": "1234",
+    "name": "Nick",
+    "role": "customer",
+    "avatar": "https://picsum.photos/800",
+    "creationAt": "2024-11-12T11:35:29.000Z",
+    "updatedAt": "2024-11-12T11:35:29.000Z"
+  },
+  {
+    "id": 30,
+    "email": "Nick@gmail.com",
+    "password": "1234",
+    "name": "Nick",
+    "role": "customer",
+    "avatar": "https://picsum.photos/800",
+    "creationAt": "2024-11-12T11:40:11.000Z",
+    "updatedAt": "2024-11-12T11:40:11.000Z"
+  }
+];
