e6680e1fab264dc0278150d6281957facf6df4a5
Sergio <santozzi@gmail.com>
2024-11-13 16:38:47 -0300

    mapping models entity

--- lib/infrastrucure/models/users_model.dart
+++ lib/infrastrucure/models/users_model.dart
@@ -1,3 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_base/domain/entities/user_entity.dart';
 
-class UsersModel {}
+class UsersModel {
+  final int id;
+  final String email;
+  final String password;
+  final String name;
+  final String role;
+  final String avatar;
+  final DateTime creationAt;
+  final DateTime updatedAt;
+
+  UsersModel({
+    required this.id,
+    required this.email,
+    required this.password,
+    required this.name,
+    required this.role,
+    required this.avatar,
+    required this.creationAt,
+    required this.updatedAt,
+  });
+
+  factory UsersModel.fromJson(Map<String, dynamic> json) {
+    return UsersModel(
+      id: json['id'],
+      email: json['email'],
+      password: json['password'],
+      name: json['name'],
+      role: json['role'],
+      avatar: json['avatar'],
+      creationAt: DateTime.parse(json['creationAt']),
+      updatedAt: DateTime.parse(json['updatedAt']),
+    );
+  }
+
+  UserEntity toUserEntity() => UserEntity(
+      id: id,
+      email: email,
+      password: password,
+      name: name,
+      role: role,
+      avatar: avatar,
+      creationAt: creationAt,
+      updatedAt: updatedAt);
+}
