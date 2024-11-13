30f39622f7e8aa4416af63617a767653c6ddcdc8
Sergio <santozzi@gmail.com>
2024-11-13 13:13:14 -0300

    screens barel

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final userRepository =
+    final UserRepository userRepository =
         UserRepositoryImp(userDatasource: MockUserDatasourceImpl());
 
     return MultiProvider(
