e89a22c522f9486e3e8d6157445746e9a5c27d16
Sergio <santozzi@gmail.com>
2024-11-13 13:14:09 -0300

    user model

--- lib/presentation/providers/users_provider.dart
+++ lib/presentation/providers/users_provider.dart
@@ -6,10 +6,24 @@ import '../../domain/repositories/user_repository.dart';
 class UsersProvider extends ChangeNotifier {
   final UserRepository usuarioRepository;
   List<UserEntity> users = [];
+  UserEntity user = UserEntity(
+      id: 0,
+      name: '',
+      email: '',
+      role: '',
+      avatar: '',
+      creationAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+      password: '');
   UsersProvider({required this.usuarioRepository});
 
   Future<void> getUsers() async {
     users = await usuarioRepository.getUsers();
+    notifyListeners();
+  }
+
+  Future<void> getUser(int id) async {
+    await usuarioRepository.getUser(id);
     notifyListeners();
   }
 
